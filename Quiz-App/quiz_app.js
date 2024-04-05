6a2f69daa950aa4e4c1e1640037cdbd041c84cd4
ModeiroTegue <mikelmodeirotegue@gmail.com>
2024-04-05 17:55:47 +0100

    Updates on debugging the errors due to DOM loading and correct referencing of elements

--- Quiz-App/quiz_app.js
+++ Quiz-App/quiz_app.js
@@ -1,21 +1,19 @@
 // First thing, we need to create a standard array of correct answers
 
 const correctAnswers =['D', 'A', 'A'];
-var score=0;
 // attach an eventListerner to form to listen when the user has submited the form 
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    const form=document.getElementsByClassName('quiz-form');
-    const totalScore=document.getElementsByClassName('result');
-    if (form){
+    const form= document.querySelector('.quiz-form');
+    const result =document.querySelector('.result');
 
-        form.addEventListener('submit', e =>{
+        form.addEventListener('submit', (e) =>{
             e.preventDefault();// prevent the submit event to execute upon refreshing the page
+
+            let score=0;
         
             const userAnswers=[form.question1.value, form.question2.value, form.question3.value];
         
             // check answers
-        
             userAnswers.forEach((answer, index)=>{
                 if (answer===correctAnswers[index]){
                     score+=30;
@@ -23,12 +21,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
             });
             // console.log(score); here I want to use javascript to show the score just under the heading QUiz App
              scrollTo(0,0);
-            totalScore.document.getElementsByTagName('span').textContent = '${score}%';
-            totalScore.classlist.remove('d-none');
+             result.querySelector('span').textContent = `${score}%`
+             console.log(score)
+             result.classList.remove('d-none')
         });
-
-    }
-})
 
 
     
+
